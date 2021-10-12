c1a850a0cd58e2fb8241ab9f61ab50df0d3cf494
Guilherme Rodrigues <guilherme.rp2010@hotmail.com>
2021-10-12 05:49:52 -0300

    feat: adding canvas drawing

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,26 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import { Navigation } from './components/Navigation'
-
-import { CreateTodo } from './pages/CreateTodo'
 import { Home } from './pages/Home'
-import { Done } from './pages/Done'
-import { Login } from './pages/Login'
 
 export const AppRoute = () => {
 
   return (
     <BrowserRouter>
-      <Navigation />
 
       <Switch>
         <Route path="/" exact>
           <Home />
-        </Route>
-
-        <Route path="/login">
-          <Login />
         </Route>
 
       </Switch>
